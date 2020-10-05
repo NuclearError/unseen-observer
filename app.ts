c395edea88a0cd98fa3e30f6574d787244b4ff0a
Angela Cullen <angela@paddle.com>
2020-10-05 10:20:17 +0100

    initial commit

---
+++ app.ts
@@ -0,0 +1,42 @@
+import express, { Request, Response } from "express";
+import { postSummariesForKeyword, findThread } from "./data/index";
+
+const app = express();
+const port = 9013;
+
+app.use(express.static("public"));
+app.set("view engine", "hbs");
+
+app.get("/", (_req: Request, res: Response) => {
+  res.redirect("/search.html");
+});
+
+app.get("/results", (req: Request, res: Response) => {
+  const keyword = req.query.keyword as string | undefined;
+  if (!keyword) {
+    res.render("results", { error: "Please provide keyword" });
+    return;
+  }
+
+  // If you want to sort by multiple aspects, rawSort type
+  // should be a string or array, instead of string or undefined.
+  // You would then need to loop over that array and process it accordingly.
+  const rawSort = req.query.sort as string | undefined;
+  const sort = rawSort === "oldest" ? rawSort : "newest";
+
+  const posts = postSummariesForKeyword(keyword, sort);
+  res.render("results", {
+    posts,
+    searchterm: keyword,
+    total: posts.length,
+  });
+});
+
+app.get("/post/:file", (req: Request, res: Response) => {
+  const index = parseInt(req.params.index);
+  res.render("thread", { posts: findThread(req.params.file) });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ data/index.ts
@@ -0,0 +1,116 @@
+interface Signature {
+  text?: string;
+  html?: string;
+  images?: string[];
+}
+
+interface PostContent {
+  text?: string;
+  html?: string;
+}
+
+interface DatePosted {
+  text: string;
+  html: string;
+  date: string;
+}
+
+export interface Post {
+  title?: string;
+  author: string;
+  byline?: string;
+  datePosted: DatePosted;
+  signature?: Signature;
+  content?: PostContent;
+}
+
+export interface Providence {
+  source: { file: string; index: number };
+}
+
+interface ScraperData {
+  file: string;
+  posts: Post[];
+}
+
+const sourcePosts = (path: string, tag: string): Array<Post & Providence> =>
+  require(path).flatMap((data: ScraperData) => {
+    return data.posts.map((post, i) => ({
+      ...post,
+      source: { file: data.file, index: i, tag },
+    }));
+  });
+
+const postsByTag = {
+  chronicles: sourcePosts("./chronicles.json", "chronicles"),
+  rumors: sourcePosts("./rumours.json", "rumors"),
+  uncategorised: sourcePosts("./uncategorised.json", "uncategorised"),
+  unofficial: sourcePosts("./unofficial.json", "unofficial"),
+};
+
+type Tag = keyof typeof postsByTag;
+
+const emptyPostsArray = (): Array<Post & Providence> => [];
+
+const collectTags = (tags: Tag[]) =>
+  emptyPostsArray().concat(...tags.map((tag) => postsByTag[tag]));
+
+// export const allPosts: Array<Post & Providence> = collectTags([
+//   "chronicles",
+//   "rumors",
+//   "uncategorised",
+//   "unofficial",
+// ]);
+
+export const postSummariesForKeyword = (
+  keyword: string,
+  startingFrom: "newest" | "oldest",
+  tags: Tag[]
+) => {
+  return collectTags(tags)
+    .flatMap(({ title, author, datePosted, content, source }) => {
+      const postText = content?.text;
+      if (
+        !postText ||
+        !postText.toLowerCase().includes(keyword.toLowerCase())
+      ) {
+        return []; // Exclude post from results
+      }
+
+      return [
+        {
+          title,
+          author,
+          datePosted,
+          source,
+          match: highlightMatch(postText, keyword),
+        },
+      ];
+    })
+    .sort(
+      (a, b) =>
+        (startingFrom === "oldest" ? 1 : -1) *
+        a.datePosted.date.localeCompare(b.datePosted.date)
+    );
+};
+
+const highlightMatch = (text: string, keyword: string): string => {
+  const location = text.toLowerCase().indexOf(keyword.toLowerCase());
+  const matchString =
+    location < 50
+      ? text.substring(0, 100) + "..."
+      : "..." + text.substring(location - 50, location + 50) + "...";
+
+  const matchedText = text.substring(location, location + keyword.length);
+  return matchString.replace(
+    new RegExp(keyword, "gi"),
+    `<strong>${matchedText}</strong>`
+  );
+};
+
+export const findThread = (file: string) => {
+  const postData = allPosts.filter((post) => post.source.file === file);
+  return postData;
+};
+
+export default allPosts;
